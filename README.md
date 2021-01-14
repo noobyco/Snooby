71665207a7cf0924165a8ab834a6cc9ece6f5a38
Md Moinuddin <moinuddinshekh2@gmail.com>
2021-01-14 08:28:53 -0800

    Signed-off-by: Md Moinuddin <moinuddinshekh2@gmail.com>
    	modified:   README.md
    	modified:   snake.py
    
    1. Added quiting functionality from Game_over and Game_loop screen.
    2. updated snake collision with apple.
    3. Added Snake self collision.
    4. Updated colors.
    5. Updated snake_step.
    6. Updated apple_size.
    7. Updated README.md.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # snake-game
-A 2D game made out of pygame[Python3]
+Admin:
+	Username:
+		Md Moinuddin
+		@Noobyco
+	Contacts:
+		Twitter: https://twitter.com/Noobyco?s=08
+	licence: MIT
+
+Decription: 
+	A 2D game made out of pygame[Python3]
+
+How to run the game:
+	You need python to run this game along with couple of modules and packages..
+	<Pygame> To install Pygame type the following command in your cmd.
+		$ pip install pygame
+		
+		*If you are facing trouble in installing pygame then you might wanna add your script folder to path. 
+
+	You could make it excutable using pyinstaller module.
+	<pyinstaller> To install pyinstaller using pip, type the following command.
+		$ pip install pyinstaller
+
+	Open up cmd at the same location where your snake-game file exist. type the following command.
+		$ pyinstaller <file-name.py>  
+		
--- snake.py
+++ snake.py
@@ -1,7 +1,8 @@
-import pygame
-import time
-import random
 from pygame.locals import *
+import pygame
+import random
+import time
+
 
 
 #Initialize..
@@ -9,32 +10,33 @@ pygame.init()
 Win_Size = [800,500]
 
 Display = pygame.display.set_mode(Win_Size)
-pygame.display.set_caption("Snake.io")
+pygame.display.set_caption("Snake-game")
 clock = pygame.time.Clock()
 
 
 
 
 ##Colors..
-red = [225,0,0]
-blue = [0,0,255]
+red = [254,5,0]
+green = [192,255,24]
+blue = [17,29,94]
 black = [0,0,0]
 white = [255,255,255]
-
+orange = [243,113,33]
 ##Font Object..
-font = pygame.font.SysFont(None, 20)
+font = pygame.font.SysFont(None, 25)
 
 def snake(block_width,block_height,SnakeList):
     for XnY in SnakeList:
-        Block = pygame.draw.rect(Display,blue,(XnY[0],XnY[1],block_width,block_height))
-def text(msg,color):
+        Block = pygame.draw.rect(Display,green,(XnY[0],XnY[1],block_width,block_height))
+def text(msg,color,x,y):
     screen_text = font.render(msg, True, color)
-    Display.blit(screen_text, [Win_Size[0]/2,Win_Size[1]/2])
+    Display.blit(screen_text, [x,y])
 
 
 
 def Game_Loop():
-##Block_Object
+    ##Block_Object
     SnakeList = []
     SnakeLength = 3
     
@@ -42,15 +44,17 @@ def Game_Loop():
     pos_y = Win_Size[1]/2
     pos_x_change = 0
     pos_y_change = 0
-    block_width = 10
-    block_height = 10
-    velocity = 10
+    snake_width = 10
+    snake_height = 10
+    
+    snake_step = 15
 
     ##Apple_Object
-    randApple_x = round(random.randrange(0,Win_Size[0]-block_width)/10.0)*10.0
-    randApple_y = round(random.randrange(0,Win_Size[1]-block_height)/10.0)*10.0
-    apple_width = 10
-    apple_height = 10
+    
+    apple_width = 20
+    apple_height = 20
+    randApple_x = round(random.randrange(0,Win_Size[0]-apple_width))
+    randApple_y = round(random.randrange(0,Win_Size[1]-apple_height))
     
     ##Game_setting
     Game_Exit = False
@@ -61,9 +65,16 @@ def Game_Loop():
 
         while Game_Over == True:
             Display.fill(black)
-            text("GAME OVER! press c to continue or q to quit.", red)
+            text("GAME OVER!", orange,Win_Size[0]/2,Win_Size[1]/2)
+            text("Press c to continue.",orange,(Win_Size[0]/2)-20,(Win_Size[1]/2)+15)
+            text("Press q to Quit.", orange,(Win_Size[0]/2)-20,(Win_Size[1]/2)+25)
+            
+            
             pygame.display.update()
             for event in pygame.event.get():
+                if event.type == QUIT:
+                    Game_Exit = True
+                    Game_Over = False
                 if event.type == KEYDOWN:
                     if event.key == K_c:
                         Game_Loop()
@@ -75,62 +86,62 @@ def Game_Loop():
 
         for event in pygame.event.get():
             if event.type == QUIT:
-                run = True
+                Game_Exit = True
 
             if event.type == KEYDOWN:
                 if event.key == K_LEFT:
-                    pos_x_change = -velocity
+                    pos_x_change = -snake_step
                     pos_y_change = 0
                 if event.key == K_RIGHT:
-                    pos_x_change = velocity
+                    pos_x_change = snake_step
                     pos_y_change = 0
                 if event.key == K_UP:
-                    pos_y_change = -velocity
+                    pos_y_change = -snake_step
                     pos_x_change = 0
                 if event.key == K_DOWN:
-                    pos_y_change = velocity
+                    pos_y_change = snake_step
                     pos_x_change = 0
                     
-                    
-            """        
-            if event.type == KEYUP:
-                if event.key == K_LEFT:
-                    moving_left = False
-                if event.key == K_RIGHT:
-                    moving_right = False
-                if event.key == K_UP:
-                    moving_up = False
-                if event.key == K_DOWN:
-                    moving_down = False
-        """
-
+        ## Wall collision...
         if pos_x < 0 or pos_x > Win_Size[0] or pos_y < 0 or pos_y > Win_Size[1]:
             Game_Over = True
 
-        if pos_x == randApple_x and pos_y == randApple_y:
-            randApple_x = round(random.randrange(0,Win_Size[0]-block_width)/10.0)*10.0
-            randApple_y = round(random.randrange(0,Win_Size[1]-block_height)/10.0)*10.0
-            SnakeLength +=1
-            #print("um yum yum ")
-
+        ## Snake Collision with Apple...     
+        if pos_x > randApple_x and pos_x < randApple_x + apple_width or pos_x + snake_width > randApple_x and pos_x + snake_width < randApple_x + apple_width:
+            if pos_y > randApple_y and pos_y < randApple_y + apple_height or pos_y + snake_height > randApple_y and pos_y + snake_height < randApple_y + apple_height:
+                randApple_x = round(random.randrange(0,Win_Size[0]-apple_width))
+                randApple_y = round(random.randrange(0,Win_Size[1]-apple_height))
+                SnakeLength +=1
+            
             
         pos_x += pos_x_change
         pos_y += pos_y_change
 
+        
+
         ##Canvas
         
-        Display.fill(white)
+        Display.fill(blue)
+
+        ## Apple Object...
         Apple = pygame.draw.rect(Display, red,(randApple_x,randApple_y,apple_width,apple_height))
-        
-        
         SnakeHead = []
         SnakeHead.append(pos_x)
         SnakeHead.append(pos_y)
         SnakeList.append(SnakeHead)
 
+        
+        
         if len(SnakeList) > SnakeLength:
             del SnakeList[0]
-        snake(block_width,block_height,SnakeList)
+
+        ## Self collision...
+        for eachSegment in SnakeList[:-3]:
+            if eachSegment == SnakeHead:
+                Game_Over = True
+                
+        ## Snake Object...    
+        snake(snake_width,snake_height,SnakeList)
         
         pygame.display.update()
         clock.tick(Fps)
