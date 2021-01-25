ac5d1769df8fe57999fd433322737f81bccf40ef
Md Moinuddin <moinuddinshekh2@gmail.com>
2021-01-25 10:52:47 -0800

    Signed-off-by: Md Moinuddin <moinuddinshekh2@gmail.com>
     On branch master
    	modified:   README.md
    	new file:   images/Apple.png
    	new file:   images/icon.png
    	new file:   images/pause.png
    	new file:   images/start.png
    	modified:   snake.py
    	new file:   sounds/GameMusic.mp3
    	new file:   sounds/GameOver.mp3
    
     Updates
    
        1. Added start_page.
        2. Added game sound.
        3. Added pause functionality.
        4. Updated Apple.
        5. Added score system.
        6. Added highest score system.
        7. Added game icon.
        8. Updated snake speed.

--- README.md
+++ README.md
@@ -1,26 +1,43 @@
-# snake-game
-Admin:
-	Username:
-		Md Moinuddin
-		@Noobyco
-	Contacts:
-		Twitter: https://twitter.com/Noobyco?s=08
-	licence: MIT
+# Snooby
 
-Decription: 
-	A 2D game made out of pygame[Python3]
+Admin		:	Md Moinuddin @Noobyco
 
-How to run the game:
+Contacts	:	Twitter: https://twitter.com/Noobyco?s=08
+
+licence		:	MIT
+
+Decription	:	A 2D game made out of pygame[Python3]
+
+
+
+Run the game	:
+
+
 	You need python to run this game along with couple of modules and packages..
-	<Pygame> To install Pygame type the following command in your cmd.
+	
+	<Pygame> To install Pygame type the following command.
+
 		$ pip install pygame
 		
-		*If you are facing trouble in installing pygame then you might wanna add your script folder to path. 
+	If you are facing trouble in installing pygame then you might wanna add your python/script folder to path.
+	
+	----------------------------------------------------------------------------------------------------------
+	
+Create executable :
 
 	You could make it excutable using pyinstaller module.
+	
 	<pyinstaller> To install pyinstaller using pip, type the following command.
+
 		$ pip install pyinstaller
 
-	Open up cmd at the same location where your snake-game file exist. type the following command.
+	Open up cmd at the same location where your "Snooby" file exist, type the following command.
+		
 		$ pyinstaller <file-name.py>  
+	
+	To create executable without console and onefile executable type following command.
+
+		$ pyinstaller <file-name.py> --onefile --noconsole
+
+	---------------------------------------------------------------------------------------------------------
 		
--- snake.py
+++ snake.py
@@ -1,44 +1,134 @@
 from pygame.locals import *
+from pygame import mixer
 import pygame
 import random
-import time
+
+
 
 
 
 #Initialize..
+
 pygame.init()
 Win_Size = [800,500]
-
+iconPath = 'images/icon.png'
+icon = pygame.image.load(iconPath)
 Display = pygame.display.set_mode(Win_Size)
-pygame.display.set_caption("Snake-game")
+pygame.display.set_caption("Snooby")
+pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
+
+
+
+##Images...
+
+startScreenPath = 'images/start.png'
+ApplePath = 'images/Apple.png'
+pausedPath = 'images/pause.png'
+
+startScreen = pygame.image.load(startScreenPath)
+Apple = pygame.image.load(ApplePath)
+paused = pygame.image.load(pausedPath)
 
 
 
 
 ##Colors..
+
 red = [254,5,0]
 green = [192,255,24]
 blue = [17,29,94]
 black = [0,0,0]
 white = [255,255,255]
 orange = [243,113,33]
-##Font Object..
-font = pygame.font.SysFont(None, 25)
+
+
+## Sounds...
+
+mixer.pre_init(44100, -16, 2, 512)
+
+gameMusicPath = 'sounds/GameMusic.mp3'
+gameOverPath = 'sounds/GameOver.mp3'
+
+mixer.music.load(gameMusicPath)
+gameover_sound = mixer.Sound(gameOverPath)
+
+    
+##Highest Score...
+        
+HS = 0
+
+
+##Font Object...
+
+font = pygame.font.SysFont("comicsansms", 30)
 
 def snake(block_width,block_height,SnakeList):
     for XnY in SnakeList:
         Block = pygame.draw.rect(Display,green,(XnY[0],XnY[1],block_width,block_height))
-def text(msg,color,x,y):
-    screen_text = font.render(msg, True, color)
-    Display.blit(screen_text, [x,y])
+
+def text_object(text,color):
+    text_area = font.render(text, True, color)
+    return text_area , text_area.get_rect()
+
+def text(msg,color):
+    surface , textRect = text_object(msg, color)
+    textRect.center = (Win_Size[0]/2),(Win_Size[1]/2)
+    Display.blit(surface, textRect)
 
 
+##Pause function...
+    
+def Pause_Screen():
+    Pause = True
+    mixer.music.fadeout(1000)
+    while Pause:
 
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_c:
+                    mixer.music.play(-1)
+                    Pause = False
+
+                if event.key == K_q:
+                    pygame.quit()
+                    quit()
+                    
+        
+        Display.blit(paused,[0,0])
+        pygame.display.update()
+        clock.tick(5)
+               
+def Start_Screen():
+    StartLoop = True
+    
+    while StartLoop == True:
+        Display.fill(white)
+        Display.blit(startScreen, [0,0])
+ 
+        
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == KEYDOWN:
+                if event.key == K_c:
+                    StartLoop = False
+
+                if event.key == K_q:
+                    pygame.quit()
+                    quit()
+        
 def Game_Loop():
-    ##Block_Object
+
+    
+    ##Snake_Object
     SnakeList = []
     SnakeLength = 3
+    
     
     pos_x = Win_Size[0]/2
     pos_y = Win_Size[1]/2
@@ -46,7 +136,6 @@ def Game_Loop():
     pos_y_change = 0
     snake_width = 10
     snake_height = 10
-    
     snake_step = 15
 
     ##Apple_Object
@@ -59,33 +148,48 @@ def Game_Loop():
     ##Game_setting
     Game_Exit = False
     Game_Over = False
-    Fps = 15
-        
-    while not Game_Exit:
+    Game_Update = True
+    Fps = 20
+    
+    mixer.music.play(-1)
+    mixer.music.set_volume(0.4)
 
+
+    while not Game_Exit:
+        global HS
+        score = (SnakeLength-3)
+        
+        if score > HS:
+            HS = score
+            
         while Game_Over == True:
             Display.fill(black)
-            text("GAME OVER!", orange,Win_Size[0]/2,Win_Size[1]/2)
-            text("Press c to continue.",orange,(Win_Size[0]/2)-20,(Win_Size[1]/2)+15)
-            text("Press q to Quit.", orange,(Win_Size[0]/2)-20,(Win_Size[1]/2)+25)
-            
-            
+            gameover_sound.set_volume(0.1)
+            gameover_sound.play()
+                        
+            text("HIGHEST SCORE : {}".format(str(HS)), white)
             pygame.display.update()
+            
             for event in pygame.event.get():
                 if event.type == QUIT:
                     Game_Exit = True
                     Game_Over = False
                 if event.type == KEYDOWN:
                     if event.key == K_c:
+                        mixer.music.play(-1)
+                        gameover_sound.fadeout(100)
                         Game_Loop()
                     if event.key == K_q:
                         Game_Exit = True
                         Game_Over = False
-                            
+                  
+            
+             
             
 
         for event in pygame.event.get():
             if event.type == QUIT:
+                mixer.music.fadeout(100)
                 Game_Exit = True
 
             if event.type == KEYDOWN:
@@ -102,8 +206,12 @@ def Game_Loop():
                     pos_y_change = snake_step
                     pos_x_change = 0
                     
+                if event.key == K_p:
+                    Pause_Screen()
+                    
         ## Wall collision...
         if pos_x < 0 or pos_x > Win_Size[0] or pos_y < 0 or pos_y > Win_Size[1]:
+            mixer.music.fadeout(1000)
             Game_Over = True
 
         ## Snake Collision with Apple...     
@@ -124,7 +232,8 @@ def Game_Loop():
         Display.fill(blue)
 
         ## Apple Object...
-        Apple = pygame.draw.rect(Display, red,(randApple_x,randApple_y,apple_width,apple_height))
+        
+        Display.blit(Apple,[randApple_x,randApple_y])
         SnakeHead = []
         SnakeHead.append(pos_x)
         SnakeHead.append(pos_y)
@@ -138,16 +247,30 @@ def Game_Loop():
         ## Self collision...
         for eachSegment in SnakeList[:-3]:
             if eachSegment == SnakeHead:
+                mixer.music.fadeout(1000)
                 Game_Over = True
+                
                 
         ## Snake Object...    
         snake(snake_width,snake_height,SnakeList)
         
+        ## Score Board...
+        if (SnakeLength-3) > 0:
+            text(str(SnakeLength-3),white)
+
+        
+
         pygame.display.update()
         clock.tick(Fps)
 
     
     pygame.quit()
     quit()
+    
+## Game call...
+    
+Start_Screen()
+
+
 
 Game_Loop()
